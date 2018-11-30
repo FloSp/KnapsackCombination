ee896c517ff37398c14921c91a779f82c4319d5f
Florian Speicher <Florian.speicher@tieto.com>
2018-11-30 10:42:44 +0100

    Added Length Class

---
+++ Combination/Length.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Combination
+{
+    public class Length
+    {
+        public BigInteger Value { get; }
+
+
+        private Length(BigInteger value)
+        {
+            this.Value = value;
+        }
+
+        public static implicit operator Length(BigInteger value)
+        {
+            return new Length(value);
+        }
+
+        public static Length operator *(BigInteger left, Length right)
+        {
+            return left * right.Value;
+        }
+
+        public static Length operator *(Length left, BigInteger right)
+        {
+            return left.Value * right;
+        }
+
+        public static Length operator +(Length left, Length right)
+        {
+            return left.Value + right.Value;
+        }
+
+        public static Length operator -(Length left, Length right)
+        {
+            return left.Value - right.Value;
+        }
+
+        public static BigInteger operator /(Length left, Length right)
+        {
+            return left.Value / right.Value;
+        }
+    }
+}
